9f0d2a64308b1ac41b8d1edc627bec1116b1509d
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-25 05:55:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,27 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+def load_library(file)
   # code goes here
+  emoticons_file = YAML.load_file(file)
+
+  emoticons_hash = {
+    get_emoticon = {},
+    get_meaning = {}
+  }
+
+  emoticons_file.each do |key, array|
+    array.each do |emoticon|
+      emoticons_hash[:get_emoticon][array.first] = array.last
+      emoticons_hash[:get_meaning][array.last] = key
+    end
+  end
+  emoticons_hash  
 end
 
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
 def get_english_meaning
